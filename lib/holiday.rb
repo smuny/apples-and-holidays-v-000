7b6dc693d2b35097b4bf9240a755b26fc5185c28
Sharon Mun <sharonymun@gmail.com>
2018-07-16 15:15:58 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -144,6 +144,7 @@ def all_holidays_with_bbq(holiday_hash)
    holiday_hash.each do |season, holidays|
      holidays.each do |holiday, supplies|
        if supplies include?("BBQ")
+         holiday_names << holiday
   end
  end
 end
