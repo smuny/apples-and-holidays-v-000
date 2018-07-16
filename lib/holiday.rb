52762b1de1c2834296c095cd1441a87daaca5cb8
Sharon Mun <sharonymun@gmail.com>
2018-07-16 15:02:11 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -112,7 +112,7 @@ def all_supplies_in_holidays(holiday_hash)
      }
    }
     holiday_hash.each do |season, holidays|
-      puts "#{holidays.capitalize}:"
+      puts "#{season.capitalize}:"
       
 end
 end
