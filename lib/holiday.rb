1982edb6c0d9f42458752b0d46b9f8201211dc21
Sharon Mun <sharonymun@gmail.com>
2018-07-16 13:22:51 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,6 +63,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
        :memorial_day => ["BBQ"]
      }
    }
+   holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
