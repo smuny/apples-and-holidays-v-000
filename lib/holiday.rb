d77fc72276d94ce33f5dc1f3a3ec991ccc635507
Sharon Mun <sharonymun@gmail.com>
2018-07-16 14:44:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -117,7 +117,6 @@ def all_supplies_in_holidays(holiday_hash)
       puts "  #{holiday.to_s.split("_").map{|word| word.capitalize}.join(" ")}: #{supplies.join(", ")
 end
 end
-end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
@@ -144,4 +143,3 @@ end
 
 all_supplies_in_holidays(holiday_hash)
 
-end
