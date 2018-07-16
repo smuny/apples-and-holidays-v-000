15a836c756eaec0f433d4d77daddc10bf730d316
Sharon Mun <sharonymun@gmail.com>
2018-07-16 15:19:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -151,21 +151,3 @@ end
 end
 
 
-holiday_hash = {
-     :winter => {
-       :christmas => ["Lights", "Wreath"],
-       :new_years => ["Party Hats"]
-     },
-     :summer => {
-       :fourth_of_july => ["Fireworks", "BBQ"]
-     },
-     :fall => {
-       :thanksgiving => ["Turkey"]
-     },
-     :spring => {
-       :memorial_day => ["BBQ"]
-     }
-   }
-
-all_supplies_in_holidays(holiday_hash)
-
